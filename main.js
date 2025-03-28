cc9bd0a85db2a20c49f3b4ffcc4d99a340b3f5ce
brennm2 <brendon_marcello@yahoo.com>
2025-03-28 19:00:45 +0000

    more updates

--- main.js
+++ main.js
@@ -1,15 +1,68 @@
 
+const secretAudio = new Audio('audio/secretAudio.mp3');
+const secretAudio2 = new Audio('audio/fireinhole.ogg');
+
+
 function getTime()
 {
 	fetch('https://timeapi.io/api/time/current/zone?timeZone=Europe%2FLisbon')
 	.then(response => response.json())
 	.then(data => {
 
-		const element = document.getElementById("example")
+		const element = document.getElementById("time")
 		const hour = String(data.hour).padStart(2, '0')
 		const minute = String(data.minute).padStart(2, '0')
-		element.innerHTML = "Current time: " + hour + ":" + minute + " - "
+
+		element.textContent = "Current time: " + hour + ":" + minute + " - "
 	})
 	.catch(error => console.error('Error fetching data:', error));
 }
+
+function playSecret()
+{
+	const newMain = document.getElementById('mainDiv');
+	const time = document.querySelector('.profileTime')
+	const middleDiv = document.querySelector('.middleDiv')
+
+	newMain.style.backgroundColor = "black"
+	time.textContent = "Current time: Rush B - "
+	if (secretAudio.paused){
+		secretAudio2.volume = 0.2
+		secretAudio2.play()
+		console.log(middleDiv.filter)
+		setTimeout(() => {
+			secretAudio.play();
+			newMain.style.backgroundImage = "url('images/backgroundSecret.jpeg')";
+			middleDiv.style.backdropFilter = 'none';
+			middleDiv.style.boxShadow = 'none';
+		}, 1200);
+	}
+}
+
+function changeProfile(newUsername, newQuote)
+{
+	document.querySelector('.userQuote').textContent = newQuote;
+	document.querySelector('.userName').textContent = '@' + newUsername;
+	document.querySelector('.profileTime').textContent = 'Getting current time...'
+	getTime()
+}
+
+function clickButton()
+{
+	document.querySelector('.boxButton').addEventListener('click', () => {
+		const newUsername = document.querySelector('.inputNick').value;
+		const newQuote = document.querySelector('.inputQuote').value;
+
+		if (newQuote && newUsername){
+			if (newUsername == "cs" && newQuote == "1.6"){
+				playSecret()
+			}
+			else{
+				changeProfile(newUsername, newQuote)
+			}
+		}
+	});
+}
 getTime()
+clickButton();
+
