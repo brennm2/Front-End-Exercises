c58ba736bec16470a3f76704b187435d239bf1b4
brennm2 <brendon_marcello@yahoo.com>
2025-03-28 09:43:48 +0000

    fixed the 0 problem in the hours

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ function getTime()
 	fetch('https://timeapi.io/api/time/current/zone?timeZone=Europe%2FLisbon')
 	.then(response => response.json())
 	.then(data => {
+
 		const element = document.getElementById("example")
-		element.innerHTML = "Current time: " + data.hour + ":" + data.minute + " - "
+		const hour = String(data.hour).padStart(2, '0')
+		const minute = String(data.minute).padStart(2, '0')
+		element.innerHTML = "Current time: " + hour + ":" + minute + " - "
 	})
 	.catch(error => console.error('Error fetching data:', error));
 }
